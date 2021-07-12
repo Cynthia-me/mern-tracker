75f57a59e66dddecbecb4d7ca2a82d280774b00c
Cynthia-me <ayecythia@gmail.com>
2021-07-12 15:56:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route,Switch} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+import Navbar from '../src/Components/Navbar'
+import ExercisesList from '../src/Components/ExercisesList'
+import ExercisesEdit from '../src/Components/ExercisesEdit'
+import CreateExercises from '../src/Components/CreateExercises'
+import NewUser from '../src/Components/NewUser'
+
+
+ function App(){
+     return(
+        <div className = "app">
+         <Router>
+             <Navbar/>
+             <Switch>
+             <Route exact path = "/" component = {ExercisesList}/>
+             <Route exact path = "/edit/:id" component = {ExercisesEdit}/>
+             <Route exact path = "/create" component = {CreateExercises}/>
+             <Route exact path = "/user" component = {NewUser}/>
+             </Switch>
+         </Router>
+         </div>
+        
+     )
+     
+ }
+
+ export default App;
